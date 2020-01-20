01febe5c8253b32b319c1cd7fdaf1147b0cc0b65
kumazawa-tky <kumazawa-tky@indetail.co.jp>
2020-01-20 09:44:55 +0900

    モジュール kensan の初期チェックイン

---
+++ kensan/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace kensan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<float> weights = new List<float>();
+
+            weights.Add(41.2f);
+            weights.Add(42.5f);
+            weights.Add(44.9f);
+            weights.Add(43.2f);
+
+            for (int i = 0; i < weights.Count; i++)
+            {
+                Console.WriteLine(weights[i]);
+            }
+        }
+    }
+}
